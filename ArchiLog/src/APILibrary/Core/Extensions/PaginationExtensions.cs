1ebe857835801b0a029914a2d2e48d27d45e85f5
pollajoel <pollajoel2017@gmail.com>
2020-11-27 16:07:57 +0100

    cache data

---
+++ ArchiLog/src/APILibrary/Core/Extensions/PaginationExtensions.cs
@@ -0,0 +1,23 @@
+﻿using APILibrary.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace APILibrary.Core.Extensions
+{
+    public static class PaginationExtensions
+    {
+        public static IQueryable<TModel> Skip<TModel>(this IQueryable<TModel> query, int offset, int limit) where TModel : ModelBase
+        {
+            int Star = offset - 1;
+            if (Star < 0)
+                Star = 0;
+
+            if (limit > query.Count())
+                limit = query.Count();
+
+            return query.Skip(Star).Take(limit - limit + 1);
+        }
+    }
+}
